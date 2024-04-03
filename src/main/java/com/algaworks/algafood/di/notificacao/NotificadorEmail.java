b04e90af5e1ff2054bba2244c2547447140b8427
RodResq <rodrigoresque@yahoo.com.br>
2024-04-02 23:24:26 -0300

    updt

--- src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/algaworks/algafood/di/notificacao/NotificadorEmail.java
@@ -1,13 +1,23 @@
 package com.algaworks.algafood.di.notificacao;
 
 import com.algaworks.algafood.di.modelo.Cliente;
-import org.springframework.context.annotation.Profile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
-@Profile("prod")
 @TipoNotificador(NivelUrgencia.URGENCIA)
 @Component
 public class NotificadorEmail implements Notificador {
+
+//    @Value("${notificador.email.host-servidor}")
+//    private String host;
+
+    @Autowired
+    Environment environment;
+
+//    @Value("${notificador.email.porta-servidor}")
+//    private Integer port;
 
     public NotificadorEmail() {
         System.out.println("Notificacao REAL");
@@ -15,6 +25,8 @@ public class NotificadorEmail implements Notificador {
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
+        System.out.println("Host: " + environment.getProperty("notificador.email.host-servidor"));
+        System.out.println("Porta" + environment.getProperty("notificador.email.porta-servidor"));
 
         System.out.printf("Notificando %s através do e-mail %s: %s\n",
                 cliente.getNome(), cliente.getEmail(), mensagem);
--- src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
+++ src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -1,36 +1,20 @@
 package com.algaworks.algafood.di.service;
 
 import com.algaworks.algafood.di.modelo.Cliente;
-import com.algaworks.algafood.di.notificacao.NivelUrgencia;
-import com.algaworks.algafood.di.notificacao.Notificador;
-import com.algaworks.algafood.di.notificacao.TipoNotificador;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
-
-//@Component
+@Component
 public class AtivacaoClienteService {
 
-    @TipoNotificador(NivelUrgencia.URGENCIA)
     @Autowired
-    private Notificador notificador;
-
-//    @PostConstruct
-    public void init() {
-        System.out.println("INIT");
-    }
-
-//    @PreDestroy
-    public void destroy() {
-        System.out.println("DETROY");
-    }
+    private ApplicationEventPublisher eventPublisher;
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
-        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+        eventPublisher.publishEvent(new ClienteAtivadoEvent(cliente));
 
     }
 
