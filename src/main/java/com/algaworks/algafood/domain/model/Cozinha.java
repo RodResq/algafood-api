7c54f846be5708bb8e5439d0334e6bd0d745908e
RodResq <rodrigoresque@yahoo.com.br>
2024-04-05 00:02:29 -0300

    jpa

---
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -0,0 +1,44 @@
+package com.algaworks.algafood.domain.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Cozinha {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Cozinha cozinha = (Cozinha) o;
+
+        return id != null ? id.equals(cozinha.id) : cozinha.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+}
