dcb1982f4a8f8567d2f1958119de42a4d9b5c08a
RodResq <rodrigoresque@yahoo.com.br>
2024-04-08 18:58:41 -0300

    HttpsStatus

--- src/main/java/com/algaworks/algafood/domain/model/CozinhasXmlWrapper.java
+++ src/main/java/com/algaworks/algafood/domain/model/CozinhasXmlWrapper.java
@@ -1,6 +1,5 @@
 package com.algaworks.algafood.domain.model;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
