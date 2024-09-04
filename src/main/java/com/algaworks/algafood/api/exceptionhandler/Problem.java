cf4380b7f74ac7d55376a7be4a9c748ba94d4d9b
RodResq <rodrigoresque@yahoo.com.br>
2024-09-04 17:37:16 -0300

    updt

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @JsonInclude(Include.NON_NULL)
 @Getter
@@ -16,7 +18,15 @@ public class Problem {
     private String type;
     private String title;
     private String detail;
-
     private String userMessage;
     private LocalDateTime timestamp;
+    private List<Field> fields;
+
+    @Getter
+    @Setter
+    @Builder
+    public static class Field {
+        private String name;
+        private String userMessage;
+    }
 }
