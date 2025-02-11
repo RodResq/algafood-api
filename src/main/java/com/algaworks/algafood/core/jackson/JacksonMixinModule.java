dd7a7f7793a72334a75c5e560b39ad541e691a49
rodrigo.resque <rodrigoresque@yahoo.com.br>
2025-02-11 13:59:37 -0300

    terça 2nd

--- src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
+++ src/main/java/com/algaworks/algafood/core/jackson/JacksonMixinModule.java
@@ -1,6 +1,9 @@
 package com.algaworks.algafood.core.jackson;
 
+import com.algaworks.algafood.api.model.mixin.CidadeMixin;
+import com.algaworks.algafood.api.model.mixin.CozinhaMixin;
 import com.algaworks.algafood.api.model.mixin.RestauranteMixin;
+import com.algaworks.algafood.domain.model.Cidade;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 
@@ -8,5 +11,7 @@ public class JacksonMixinModule extends SimpleModule {
 
     JacksonMixinModule() {
         setMixInAnnotation(Restaurante.class, RestauranteMixin.class);
+        setMixInAnnotation(Cidade.class, CidadeMixin.class);
+        setMixInAnnotation(CozinhaMixin.class, CozinhaMixin.class);
     }
 }
